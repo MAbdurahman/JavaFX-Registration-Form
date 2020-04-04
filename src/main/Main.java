5c54d5282485802bbabd657bf2b2e424f8f4831b
MAbdurahman <mdbdrrhm@gmail.com>
2020-04-04 07:01:50 -0500

    Modified to reflect MVC Pattern and added form icon

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        try {
+            Image icon = new Image(Main.class.getResourceAsStream("/img/form-icon.png"));
+            primaryStage.getIcons().add(icon);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        primaryStage.setTitle(" Registration Form");
+        primaryStage.setScene(new Scene(root, 600, 350));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
